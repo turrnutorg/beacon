b513331d0168e142ac29f852b04700556319f90a
turrnut <victorconqueror021@gmail.com>
2025-03-28 19:31:15 -0400

    Update install-packages.sh and grub.cfg

--- install-packages.sh
+++ install-packages.sh
@@ -5,5 +5,3 @@ sudo apt-get install binutils
 sudo apt-get install libc6-dev-i386
 sudo apt-get install xorriso
 sudo apt-get install grub
-sudo apt-get install mtools
-sudo apt-get install dosfstools
